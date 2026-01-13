cc24c82c522b9ad2c63df9f9bec0331c3c6ad5ac
HexCatDev <215917491+HexCatDev@users.noreply.github.com>
2026-01-13 23:26:54 +0100

    backup update

--- Hodiny/main.c
+++ Hodiny/main.c
@@ -5,11 +5,10 @@
  * @date 2026-01-08
  * @brief 
  */
-
 #include <builtins.h>
-#include <stdio.h>
 #include <xc.h>
 #include <pic18f47q43.h>
+#include <string.h>
 // Configuration bits for PIC18F47Q43
 #pragma config FEXTOSC = OFF           // External Oscillator not enabled
 #pragma config RSTOSC = HFINTOSC_1MHZ  // Power-on reset uses HFINTOSC 1MHz
@@ -17,32 +16,17 @@
 #pragma config MCLRE = INTMCLR         // MCLR pin disabled, RE3 input enabled
 #define _XTAL_FREQ 8000000             // 8 MHz internal oscillator frequency
 
-void CLK(int time[3])
-{
-    int h = time[0], m = time[1], s = time [2];
-    while (1) {
-        LATDbits.LATD7 = 1;
-        s++;
-        //__delay_ms(250);
-        LATDbits.LATD7 = 0;
-        //__delay_ms(250);
-        if (s >= 60) {
-            s = 0;
-            m++;
-        }
-        if (m >= 60) {
-            m = 0;
-            h++;
-        }
-        if (h >= 24) {
-            h = 0;
-        }
-    }
-    time[1] = h;
-    time[3] = m;
-    time[2] = s;
+enum Segments { DPS, As, BS, CS, DS, ES, FS, GS };
 
-    return;
+void display_digit(const int segments[8]){
+    LATDbits.LATD7 = segments[DPS] & 0x1;
+    LATDbits.LATD6 = segments[As] & 0x1;
+    LATDbits.LATD5 = segments[BS] & 0x1;
+    LATDbits.LATD4 = segments[CS] & 0x1;
+    LATCbits.LATC7 = segments[DS] & 0x1;
+    LATCbits.LATC6 = segments[ES] & 0x1;
+    LATCbits.LATC5 = segments[FS] & 0x1;
+    LATDbits.LATD3 = segments[GS] & 0x1;
 }
 
 int main(void)
@@ -73,35 +57,35 @@ int main(void)
     TRISCbits.TRISC5 = 0;
 
 
-    int Segments[8] = {0, 1, 2, 3, 4, 5 , 6, 7};
 
     int DP = LATDbits.LATD7;
     int A = LATDbits.LATD6;
     int B = LATDbits.LATD5;
     int C = LATDbits.LATD4;
-    int D = LATDbits.LATD3;
-    int E = LATCbits.LATC7;
-    int F = LATCbits.LATC6;
-    int G = LATCbits.LATC5;
+    int D = LATCbits.LATC7;
+    int E = LATCbits.LATC6;
+    int F = LATCbits.LATC5;
+    int G = LATDbits.LATD3;
 
     
     int numbers[10][8] = {
-        {0, 1, 1, 1, 1, 1, 1, 0}, //0
-        {0, 0, 1, 1, 0, 0, 0, 0}, //1
-        {0, 1, 1, 0, 1, 1, 0, 1}, //2
-        {0, 1, 1, 1, 1, 0, 0, 1}, //3
-        {0, 0, 1, 1, 0, 0, 1, 1}, //4
+        {0, 0, 1, 1, 1, 1, 1, 1}, //0
+        {0, 0, 0, 0, 0, 1, 1, 0}, //1
+        {0, 1, 0, 1, 1, 0, 1, 1}, //2
+        {0, 1, 0, 0, 1, 1, 1, 1}, //3
+        {0, 1, 1, 0, 0, 1, 1, 0}, //4
         {0, 1, 1, 0, 1, 1, 0, 1}, //5
-        {0, 1, 0, 1, 1, 1, 1, 1}, //6
-        {0, 1, 1, 1, 0, 0, 0, 0}, //7 není hotová
+        {0, 1, 1, 1, 1, 1, 0, 1}, //6
+        {0, 0, 0, 0, 0, 1, 1, 1}, //7
         {0, 1, 1, 1, 1, 1, 1, 1}, //8
-        {0, 1, 1, 1, 1, 0, 1, 1} //9
-    };      // DP,    A,      B,    C,      D,    E,     F,     G
-    int time[3];
-    CLK(time);
-    // kód
-    for (int j = 0; j <= 1; j++) {
+        {0, 1, 1, 0, 1, 1, 1, 1} //9
+    };      // DP,    G,      F,    E,      D,    C,     B,     A
+
+ 
+    // loop
+    while (1) {
         for (int i = 0; i < 6; i++) {
+            __delay_us(250);
             LATBbits.LATB0 = 1; //nastaví všechny tranzistory na high takže se vypnou protože PNP
             LATBbits.LATB1 = 1;
             LATBbits.LATB2 = 1;
@@ -110,30 +94,16 @@ int main(void)
             LATBbits.LATB5 = 1;
             // nastaví určitý tranzistor na low takže se zapne
             switch (i) {
-                case 0: LATBbits.LATB0 = 0; break;
-                case 1: LATBbits.LATB1 = 0; break;
-                case 2: LATBbits.LATB2 = 0; break;
-                case 3: LATBbits.LATB3 = 0; break;
-                case 4: LATBbits.LATB4 = 0; break;
-                case 5: LATBbits.LATB5 = 0; break;
+                case 0: LATBbits.LATB5 = 0; break;
+                case 1: LATBbits.LATB4 = 0; break;
+                case 2: LATBbits.LATB3 = 0; break;
+                case 3: LATBbits.LATB2 = 0; break;
+                case 4: LATBbits.LATB1 = 0; break;
+                case 5: LATBbits.LATB0 = 0; break;
             }
-        }  
-            //tady se budou zadávat číslice asi...
-            //tady jsou všechny segmenty nasatveny na na 0 krom DP
-        for (int i = 0; i < 8; i++) {
-            switch(Segments[i]) {
-                case 0: LATDbits.LATD7 = 1; break;
-                case 1: LATDbits.LATD6 = 0; break;
-                case 2: LATDbits.LATD5 = 0; break;
-                case 3: LATDbits.LATD4 = 0; break;
-                case 4: LATDbits.LATD3 = 0; break;
-                case 5: LATCbits.LATC7 = 0; break;
-                case 6: LATCbits.LATC6 = 0; break;
-                case 7: LATCbits.LATC5 = 0; break;
-            }
-            //__delay_ms(100);
-        }
-    }
-
+            display_digit(numbers[3]);
+            __delay_us(250);
+        };
+    } 
     return 0;
 }
